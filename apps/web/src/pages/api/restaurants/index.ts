33c49372d90133fb6a0a29ff7c2a0db526367dc3
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2023-03-22 15:44:43 +0000

    fix: axios cors errors and set the ports

--- apps/web/src/pages/api/restaurants/index.ts
+++ apps/web/src/pages/api/restaurants/index.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import type { Restaurant } from './data'
-import { MOCK_RESTAURANTS as restaurants } from './data'
+import type { Restaurant } from '../data'
+import { MOCK_RESTAURANTS as restaurants } from '../data'
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse<Restaurant[]>) {
   // Get data from your database
