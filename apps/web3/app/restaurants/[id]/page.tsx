2a8c14e8e6c31f890d2f6e9a43f0141113878ed4
Michael Kim <michaelkim39@gmail.com>
2023-03-23 17:09:46 +0000

    feat: add server components implementation of details page on web3

---
+++ apps/web3/app/restaurants/[id]/page.tsx
@@ -0,0 +1,44 @@
+import axios from 'axios'
+import React from 'react'
+import { Restaurant } from '../../api/data'
+import Image from 'next/image'
+import { GetServerSidePropsContext } from 'next'
+
+export const dynamic = 'force-dynamic'
+
+const getRestaurantDetails = async (restaurantId: string) => {
+  try {
+    const { data: restaurant } = await axios.get(
+      `http://localhost:3002/api/restaurants/${restaurantId}`
+    )
+    return restaurant
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default async function DetailsPage({ params }: GetServerSidePropsContext) {
+  const restaurant: Restaurant = await getRestaurantDetails(params?.id as string)
+
+  if (!restaurant) {
+    return null
+  }
+
+  return (
+    <div className="flex justify-center gap-5 p-16">
+      <div className="flex w-1/3 flex-col gap-5">
+        <h1 className="text-3xl">{restaurant.name}</h1>
+        <p>Cuisine: {restaurant.cuisine}</p>
+        <p>Address: {restaurant.address}</p>
+        <p>
+          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem
+        </p>
+      </div>
+      <div className="overflow-hidden rounded-lg">
+        <Image src={restaurant.image_url} alt={'candles'} width={500} height={500} />
+      </div>
+    </div>
+  )
+}
--- apps/web3/components/Card/CardButton.tsx
+++ apps/web3/components/Card/CardButton.tsx
@@ -1,14 +1,19 @@
 'use client'
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation'
 
 interface Props {
   restaurantId: number
 }
 
 export const CardButton = ({ restaurantId }: Props) => {
-  const router = useRouter();
+  const router = useRouter()
 
   return (
-    <button className="inline-flex items-center border focus:outline-none font-medium shadow-sm border-transparent bg-indigo-600 text-white hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 rounded-md px-4 py-2 text-sm" onClick={() => router.push(`/details/${restaurantId}`)}>Details</button>
+    <button
+      className="inline-flex items-center border focus:outline-none font-medium shadow-sm border-transparent bg-indigo-600 text-white hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 rounded-md px-4 py-2 text-sm"
+      onClick={() => router.push(`/restaurants/${restaurantId}`)}
+    >
+      Details
+    </button>
   )
 }
